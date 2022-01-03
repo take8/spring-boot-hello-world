3f6863d1337170c052632b6c881765a4fc8a715a
take8 <37664118+take8@users.noreply.github.com>
2022-01-03 21:58:19 +0900

    Update: Change to shortcut annotation (@LocalServerPort)

--- src/test/java/net/take8/springboothelloworld/SpringBootHelloWorldApplicationTests.java
+++ src/test/java/net/take8/springboothelloworld/SpringBootHelloWorldApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,7 +20,8 @@ class SpringBootHelloWorldApplicationTests {
   @Autowired
   private TestRestTemplate restTemplate;
 
-  @Value("${local.server.port}")
+  // @Value("${local.server.port}")
+  @LocalServerPort
   int port;
 
   @Test
