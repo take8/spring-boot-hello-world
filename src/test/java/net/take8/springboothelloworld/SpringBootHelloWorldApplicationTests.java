52b74454a7f498e6ba0aa7740e03ff4d34e5489c
take8 <37664118+take8@users.noreply.github.com>
2022-01-03 21:52:19 +0900

    Update: Add Hello Controller Test

--- src/test/java/net/take8/springboothelloworld/SpringBootHelloWorldApplicationTests.java
+++ src/test/java/net/take8/springboothelloworld/SpringBootHelloWorldApplicationTests.java
@@ -1,13 +1,35 @@
 package net.take8.springboothelloworld;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+// ポートをランダムにする
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 class SpringBootHelloWorldApplicationTests {
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Value("${local.server.port}")
+	int port;
+
 	@Test
-	void contextLoads() {
+	public void testHello() {
+		String body = this.restTemplate.getForObject("http://localhost:" + port, String.class);
+		assertThat(body).isEqualTo("Hello World!");
 	}
+
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
