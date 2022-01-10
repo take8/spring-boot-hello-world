98fc4b15c18f3a8a8d4c273cf71b9f2e8516e7a3
take8 <37664118+take8@users.noreply.github.com>
2022-01-10 16:49:55 +0900

    Update: Use JDBC

--- src/main/java/net/take8/springboothelloworld/MessagesController.java
+++ src/main/java/net/take8/springboothelloworld/MessagesController.java
@@ -1,5 +1,7 @@
 package net.take8.springboothelloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,17 +13,21 @@ import java.util.concurrent.CopyOnWriteArrayList;
 @RestController
 @RequestMapping("messages")
 public class MessagesController {
-  // 永続化しないでメモリ上に格納する
-  final List<Message> messages = new CopyOnWriteArrayList<>();
+  @Autowired
+  JdbcTemplate jdbcTemplate;
 
   @RequestMapping(method = RequestMethod.GET)
   public List<Message> getMessages() {
-    return messages;
+    return jdbcTemplate.query("SELECT text FROM messages ORDER BY id", (rs, i) -> {
+      Message m = new Message();
+      m.setText(rs.getString("text"));
+      return m;
+    });
   }
 
   @RequestMapping(method = RequestMethod.POST)
   public Message postMessages(@RequestBody Message message) {
-    messages.add(message);
+    jdbcTemplate.update("INSERT INTO messages(text) VALUES(?)", message.getText());
     return message;
   }
 }
